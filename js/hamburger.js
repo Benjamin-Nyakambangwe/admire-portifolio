9b27bd2eb622fb2f9fe389b8fa72a76133b61d58
Benjamin Nyakambangwe <benjaminnyakambangwe@gmail.com>
2020-09-21 12:17:30 +0200

    added responsive for 360 devices

---
+++ js/hamburger.js
@@ -0,0 +1,18 @@
+const openNavLink = document.getElementById('open-nav-link');
+const nav = document.getElementById('navbar');
+
+
+openNavLink.addEventListener('click', openNav);
+
+
+//Open Nav on mobile
+function openNav() {
+  console.log('Hamburger Woking');
+
+  if (nav.style.display != 'none') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
+    console.log('else working');
+  }
+}
